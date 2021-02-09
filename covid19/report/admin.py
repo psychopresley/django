b645d82e8222cb37f4009ffbb17916c9a91a780a
psychopresley <psycho.presley@gmail.co>
2021-02-09 09:06:17 -0300

    manual commit 09/02/2021

--- covid19/report/admin.py
+++ covid19/report/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from report.models import Country, CountryInfo, CountryReport
 
 # Register your models here.
+admin.site.register(Country)
+admin.site.register(CountryInfo)
+admin.site.register(CountryReport)
--- covid19/report/models.py
+++ covid19/report/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=60,unique=True,default='undefined')
+
+    def __str__(self):
+        return self.name
+
+
+class CountryInfo(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    region = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.country
+
+
+class CountryReport(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    date = models.DateField()
+    confirmed = models.IntegerField()
+    deaths = models.IntegerField()
+    recovered = models.IntegerField(default=0)
+    active = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '%s %s' % (self.country, str(self.date))
