21b065a36fa928ada01559f2245ae3db40a11afc
psychopresley <psycho.presley@gmail.co>
2021-03-01 18:34:11 -0300

    manual commit 01/03/21

--- covid19/report/forms.py
+++ covid19/report/forms.py
@@ -3,7 +3,8 @@ from django.core import validators
 from report.models import Country
 
 COUNTRIES = Country.objects.all()
-COUNTRIES_LIST = [(x.name,x.name) for x in COUNTRIES];
+COUNTRIES_LIST = list([(x.name,x.name) for x in COUNTRIES]);
+COUNTRIES_LIST.sort()
 
 class SelectCountry(forms.Form):
     country= forms.CharField(label='Select country', label_suffix=': ',
